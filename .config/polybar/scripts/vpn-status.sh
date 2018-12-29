ebab6d0bb15472d030190b7e060b73c309d2959e
sourcecube <chris@sourcecube.de>
2018-12-29 19:59:58 +0100

    fixed vpn script

--- .config/polybar/scripts/vpn-status.sh
+++ .config/polybar/scripts/vpn-status.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 PVPN_STATUS=$(sudo pvpn status)
-OPENVPN_STATUS=$(echo "$PVPN_STATUS" | egrep '\[OpenVPN Status\]:\s[A-Za-z]+' | awk '{print $NF}')
-INTERNET_STATUS=$(echo "$PVPN_STATUS" | egrep '\[Internet Status\]:\s[A-Za-z]+' | awk '{print $NF}')
+OPENVPN_STATUS=$(echo "$PVPN_STATUS" | egrep '\[OpenVPN Status\]:\s[A-Za-z]+' | awk -F ':' '{print $2}')
+INTERNET_STATUS=$(echo "$PVPN_STATUS" | egrep '\[Internet Status\]:\s[A-Za-z]+' | awk -F ':' '{print $2}')
 
-if [ "$OPENVPN_STATUS" = "Running" -a "$INTERNET_STATUS" = "Online" ]; then
-  echo "%{F#20c20e}"
-else
+if [[ "$OPENVPN_STATUS" == *"Not"* || "$INTERNET_STATUS" == *"Offline"* ]]; then
   echo "%{F#ff5555}"
+else
+  echo "%{F#20c20e}"
 fi
 
